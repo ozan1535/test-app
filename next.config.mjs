5868013407c64abb791617bf21e65e79195b9e61
ozan1535 <ozanbilgic1535@gmail.com>
2024-07-11 14:27:45 +0300

    firebase hosting

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["firebasestorage.googleapis.com","lh3.googleusercontent.com"],
+    domains: ["firebasestorage.googleapis.com", "lh3.googleusercontent.com"],
   },
-  typescript:{
+  typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
     // !! WARN !!
     ignoreBuildErrors: true,
-  }
+  },
 };
 
 export default nextConfig;
