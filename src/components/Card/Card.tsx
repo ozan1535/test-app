805f4238a4897157019b2f3a6925d96c6322814f
ozan1535 <ozanbilgic1535@gmail.com>
2024-07-14 12:53:17 +0300

    Refactor card and add border radius

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { generateFolderName } from "@/helpers/helpers";
 import Image from "next/image";
 import Link from "next/link";
+import { generateFolderName } from "@/helpers/helpers";
 import { getCardEmoji } from "./Card.helpers";
 import { IFormItems } from "@/app/types";
 import { testCategories } from "../Header/headerItems";
@@ -11,17 +11,17 @@ export default function Card({ test }: { test: IFormItems }) {
 
   return (
     <Link href={`/test/${generateFolderName(test.title)}`} className="group" cy-item="card">
-      <div className="group relative bg-white flex flex-col lg:flex-row w-full h-auto lg:h-44 rounded shadow-md">
-        <div className="flex justify-center items-center w-full lg:w-1/2 h-40 lg:h-full">
+      <div className="group relative bg-white flex flex-col lg:flex-row w-full h-auto lg:h-44 shadow-md rounded-lg">
+        <div className="flex justify-center items-center w-full lg:w-1/2 h-40 lg:h-full rounded-t-lg lg:rounded-l-lg">
           <Image
             src={test.mainMediaUrl}
             width={200}
             height={90}
             alt="Test media"
-            className="object-cover w-full h-full"
+            className="object-cover w-full h-full rounded-t-lg lg:rounded-l-lg"
           />
         </div>
-        <div className="flex flex-col p-4 justify-evenly gap-3 w-full lg:w-2/3 h-auto lg:h-full bg-white">
+        <div className="flex flex-col px-4 py-2 justify-between gap-3 w-full lg:w-2/3 h-auto lg:h-full bg-white rounded-b-lg lg:rounded-b-none lg:rounded-r-lg">
           <div>
             <p className="group-hover:text-blue-500 font-bold text-lg">{test.title}</p>
             <span className="text-xs text-slate-500 block text-ellipsis">
@@ -30,7 +30,7 @@ export default function Card({ test }: { test: IFormItems }) {
           </div>
           <p className="text-slate-500 text-xs">{categoryName}</p>
         </div>
-        <div className="absolute rounded-full w-10 h-10 -left-2 -top-2 bg-white shadow-md">
+        <div className="absolute rounded-full w-10 h-10 -left-2 -top-2">
           <Image
             src={getCardEmoji(test.category)}
             width={50}
