0e49c167200b05a473bc41d9ff2bb131722ce31c
ozan1535 <ozanbilgic1535@gmail.com>
2024-07-26 22:42:51 +0300

    Add metadata for astrology page

--- src/app/astrology/page.tsx
+++ src/app/astrology/page.tsx
@@ -1,6 +1,12 @@
+import { Metadata } from "next";
 import AstrologyItems from "@/components/AstrologyItems/AstrologyItems";
 
-export default function Astrology() {
+export const metadata: Metadata = {
+    title: 'Testarot - Astrology',
+    description: 'Discover daily, weekly, and monthly horoscopes, zodiac sign insights, and personalized astrological readings on our Astrology page. Unlock the secrets of the stars and your future today!',
+    keywords: "astrology, horoscopes, zodiac signs, astrological readings, daily horoscope, weekly horoscope, monthly horoscope, astrology insights, personalized astrology"
+}
 
+export default function Astrology() {
     return <AstrologyItems />
 }
