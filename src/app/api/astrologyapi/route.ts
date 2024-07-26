a1d04fc503623397434cf3e2e508b98f651017da
ozan1535 <ozanbilgic1535@gmail.com>
2024-07-26 19:51:19 +0300

    Throw error

--- src/app/api/astrologyapi/route.ts
+++ src/app/api/astrologyapi/route.ts
@@ -3,10 +3,19 @@ export const GET = async (req) => {
         const searchParams = new URLSearchParams(new URL(req.url).searchParams);
         const timerange = searchParams.get("timerange");
         const zodiac = searchParams.get("zodiac");
-        const res = await fetch(`https://horoscope-app-api.vercel.app/api/v1/get-horoscope/${timerange}?sign=${zodiac}`)
-        const data = await res.json()
-        return Response.json(data);
+        try {
+            const res = await fetch(`https://horoscope-app-api.vercel.app/api/v1/get-horoscope/${timerange}?sign=${zodiac}`)
+            if (res.ok) {
+                const data = await res.json()
+                return Response.json(data);
+            } else {
+                throw new Error("Something happened");
+
+            }
+        } catch (error) {
+            return Response.json({ error });
+        }
     } catch (error) {
-        return Response.json({ error: "error" });
+        return Response.json({ error });
     }
 };
